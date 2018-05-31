4857679e01a05759f3bc6b1ca0c11d32dbef071f
brunobriccola4 <brunobriccola4@gmail.com>
2018-05-31 12:04:52 -0300

    press button changes color

--- clase 2018-05-31/app.js
+++ clase 2018-05-31/app.js
@@ -1,1 +1,15 @@
 console.log('Hola');
+
+var changeColor = function (li) {
+	li.style.backgroundColor = '#F00'
+}
+
+window.onload = function () {
+	var btnPlay = document.getElementById('play');
+	btnPlay.onclick = function (){
+	var liAlives = document.getElementsByClassName('alive')
+	for (var i = liAlives.length - 1; i >= 0; i--) {
+		changeColor(liAlives[i])
+	}
+	}
+}
