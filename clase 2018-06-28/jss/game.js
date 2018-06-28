899d4e8e59a4e4154316b6686366a31f286f31e6
brunobriccola4 <brunobriccola4@gmail.com>
2018-06-28 10:04:10 -0300

    play button

--- clase 2018-06-28/jss/game.js
+++ clase 2018-06-28/jss/game.js
@@ -1,5 +1,6 @@
 var Game = {
 	next: null,
+	play: null,
 	board: null,
 	cells: null,
 	init: function () {
@@ -7,14 +8,16 @@ var Game = {
 		Game.Board = Board.getBoard();
 		Game.cells = Board.getCells();
 		Game.next = document.getElementById('next');
+		Game.play = document.getElementById('play');
 	},
 	start: function () {
 		for (var i = 0; i < Game.cells.length; i++){
 			Game.cells[i].onclick = Cell.toggle;
 		}
 		Game.next.onclick = Board.nextStep;
+		Game.play.onclick = Game.update;
 	},
 	update: function () {
-
+		setInterval(Board.nextStep, 1000);
 	}
 };
